6b894c1e42d8d8862fc7970d1ee30f0ff012ab70
Rayne <pluvstream@gmail.com>
2026-01-16 17:15:22 +0900

    Create Gradle Project

---
+++ src/main/java/dev/coma/spring/intro/SpringKyhIntroStudyApplication.java
@@ -0,0 +1,13 @@
+package dev.coma.spring.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKyhIntroStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKyhIntroStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/coma/spring/intro/SpringKyhIntroStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.coma.spring.intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKyhIntroStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
